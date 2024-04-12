eb76a7dd8500ca8bcc8ec724c5bae446a22a1a17
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-04-12 12:16:19 +0100

    creado except1

---
+++ except1.py
@@ -0,0 +1,13 @@
+import random
+def intDiv(a:int, b:int)->int:
+    try:
+        return a // b
+    except:
+        print("Realizamos tareas de control de cierre")
+
+#Comprobamos si es el flujo principal
+if __name__ == "__main__":
+    for i in range(30):
+        a = random.randint(0, 9)
+        b = random.randint(0, 9)
+        print("a:", a, "b:", b, "a // b:", intDiv(a, b))
